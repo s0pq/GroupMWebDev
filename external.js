b3a641b81b95881f1ff9494ab36602d7e4cb8347
Wiktoria Socha <wiktoriasocha236@gmail.com>
2024-12-04 12:13:19 +0000

    Add files via upload

--- external.js
+++ external.js
@@ -1,3 +1,43 @@
-// form validation will go here
+// form validation - by WS - used across all webpages that contain a form
+
+function contactUs() {
+	let formy = true; //formy is our flag 
+	let msg = "Form not completed: ";
+	//first name validation
+	if(document.getElementById("fn").value == "") {
+		msg+="You must enter your First Name. "; //we build the msg
+		formy = false; //the flag is now false
+	}
+	//surname validation
+	if(document.getElementById("sn").value == "") {
+		msg+="You must enter your Surname. "; //we build the msg
+		formy = false; //the flag is now false
+		
+	}
+	//email validation
+	if(document.getElementById("email").value == "") {
+		msg+="You must enter your email address. ";
+		formy = false;
+	}else if(document.getElementById("email").value != "") {
+		inputE = document.getElementById("email").value;
+		let symbol = inputE.indexOf("@");
+		if(symbol < 1) {
+			formy = false;
+			msg+= "Invalid email adress. ";
+		}
+	}
+	//mobile validation 
+	if(document.getElementById("phone").value == 0) {
+		msg+="You must enter your mobile number. ";
+		formy = false;
+	}
+	
+	//if formy ==  false, display the texts given
+	if(!formy) {
+		document.getElementById("details").innerHTML = msg;
+	}
+	return formy;
+	
+}
 // arrays too
 //randomisation which i have no idea what we'll do for that but we'll figure it out
